a52a7b739758a120c7a42ee6e78a24f7fecde68e
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-09-14 10:57:35 -0400

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -23,16 +23,9 @@ def db_init_users():
     conn.close()
 
 
-def db_init_posts():
 
-    conn = sqlite3.connect('db_posts.sqlite')
-    c = conn.cursor()
-    c.execute("CREATE TABLE posts (date date, username text, text text)")
-
-    conn.commit()
-    conn.close()
 
 
 if __name__ == '__main__':
     db_init_users()
-    db_init_posts()
+
