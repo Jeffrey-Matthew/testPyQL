a107ec1188e2a33ffe6fdada8f7b85288c698784
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-09-14 10:11:21 -0400

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -8,11 +8,11 @@ def db_init_users():
         ('elliot', '123123123'),
         ('tim', '12345678')
     ]
-    conn_i = sqlite3.connect('db_users.sqlite')
-    c_val = conn_i.cursor()
-    c_val.execute("CREATE TABLE users (username text, password text, failures int, mfa_enabled int, mfa_secret text)")
-    conn_i.commit()
-    conn_i.close()
+    conn = sqlite3.connect('db_users.sqlite')
+    c = c.cursor()
+    c.execute("CREATE TABLE users (username text, password text, failures int, mfa_enabled int, mfa_secret text)")
+    conn.commit()
+    conn.close()
 
 def db_init_posts():
     conn = sqlite3.connect('db_posts.sqlite')
