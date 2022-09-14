bcf2ab446d22c80396d142f68a44f6c7f4520abe
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-09-14 09:44:24 -0400

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -8,13 +8,13 @@ def db_init_users():
         ('elliot', '123123123'),
         ('tim', '12345678')
     ]
-    conn = sqlite3.connect('db_users.sqlite')
-    c = conn.cursor()
-    c.execute("CREATE TABLE users (username text, password text, failures int, mfa_enabled int, mfa_secret text)")
+    conn_i = sqlite3.connect('db_users.sqlite')
+    c_val = conn_i.cursor()
+    c_val.execute("CREATE TABLE users (username text, password text, failures int, mfa_enabled int, mfa_secret text)")
     for u,p in users:
-        c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(u, p, 0, 0, ''))
-    conn.commit()
-    conn.close()
+        c_val.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(u, p, 0, 0, ''))
+    conn_i.commit()
+    conn_i.close()
 
 def db_init_posts():
     conn = sqlite3.connect('db_posts.sqlite')
@@ -23,10 +23,6 @@ def db_init_posts():
     conn.commit()
     conn.close()
 
-if __name__ == '__main__':
-    try:
-        os.remove('db_users.sqlite')
-    try:
-        os.remove('db_posts.sqlite')
+if __name__ == '__main__':  
     db_init_users()
     db_init_posts()
